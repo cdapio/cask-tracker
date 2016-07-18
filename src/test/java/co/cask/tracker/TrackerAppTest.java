d21d84a98d706094ae436d661f751031c41df733
Yue Gao <Yue@Casks-MacBook-3.local>
2016-07-18 13:55:24 -0700

    change the addpreferredtags and add tense test for it

--- src/test/java/co/cask/tracker/TrackerAppTest.java
+++ src/test/java/co/cask/tracker/TrackerAppTest.java
@@ -65,6 +65,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -232,11 +233,13 @@ public class TrackerAppTest extends TestBase {
 
   @Test
   public void testValidate() throws Exception {
-    String response = getServiceResponse(trackerServiceManager, "v1/tags/validate",
-                                         "POST", TEST_JSON_TAGS, HttpResponseStatus.OK.getCode());
+    Random rng = new Random();
+    List<String> testList = generateStringList(rng, "abc123", 3000);
+    String response = getServiceResponse(trackerServiceManager, "v1/tags/promote",
+                                         "POST", GSON.toJson(testList), HttpResponseStatus.OK.getCode());
     ValidateTagsResult result = GSON.fromJson(response, ValidateTagsResult.class);
-    Assert.assertEquals(3, result.getValid());
-    Assert.assertEquals(1, result.getInvalid());
+    Assert.assertNotEquals(3001, result.getValid());
+    Assert.assertNotEquals(3001, result.getInvalid());
   }
 
   @Test
@@ -383,6 +386,23 @@ public class TrackerAppTest extends TestBase {
     return response;
   }
 
+  private List<String> generateStringList(Random rng, String characters, int stringNum) {
+    List<String> list = new ArrayList<>();
+    for (int i =0; i<stringNum; i++) {
+      list.add(generateString(rng, characters, rng.nextInt(60)));
+    }
+    return list;
+  }
+
+  private String generateString(Random rng, String characters, int length)
+  {
+    char[] text = new char[length];
+    for (int i = 0; i < length; i++)
+    {
+      text[i] = characters.charAt(rng.nextInt(characters.length()));
+    }
+    return new String(text);
+  }
 
   // Adapted from https://wiki.cask.co/display/CE/Audit+information+publishing
   private List<AuditMessage> generateTestData() {
